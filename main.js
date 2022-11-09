c8df5344fdd3ecb77d9070c7f574a1e031048d1f
Gustavo Henrique <110001359+gusthpm@users.noreply.github.com>
2022-11-09 06:57:43 -0300

    Update main.js

--- main.js
+++ main.js
@@ -22,11 +22,10 @@ function createCard(date, day, games) {
 }
 
 document.querySelector("#cards").innerHTML =
-  createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
-  createCard(
-    "28/11",
-    "segunda",
-    createGame("switzerland", "13:00", "brazil") +
-      createGame("portugal", "16:00", "uruguai")
-  ) +
-  createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))
+  createCard("24/11", "quinta",
+     createGame("brazil", "16:00", "serbia")) +
+  createCard("28/11","segunda",
+     createGame("switzerland", "13:00", "brazil") +
+     createGame("portugal", "16:00", "uruguai")) +
+  createCard("02/12", "sexta",
+     createGame("brazil", "16:00", "cameroon"))
